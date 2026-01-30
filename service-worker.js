087b7af2448f3ee46d3ef54db94cb13c9cf236bd
rudrsingh001 <rudrsingh0223@gmail.com>
2026-01-30 08:00:52 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,7 @@
-const CACHE_NAME = "luckydraw-v1";
-
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./service-worker.js",
-  "./style.css",
-  "./script.js",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
-  );
-});
-
-self.addEventListener("install", function () {
+self.addEventListener("install", () => {
   console.log("Service Worker Installed");
 });
 
-self.addEventListener("fetch", function () {
-  // future offline support
+self.addEventListener("activate", () => {
+  console.log("Service Worker Activated");
 });
