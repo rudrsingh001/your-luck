03afb79599b096f76380fe484108ffc48cae6f19
rudrsingh001 <rudrsingh0223@gmail.com>
2026-01-31 07:11:33 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,5 @@
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-});
+self.addEventListener("install", () => self.skipWaiting());
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("fetch", (event) => {
-  // no caching for now
 });
